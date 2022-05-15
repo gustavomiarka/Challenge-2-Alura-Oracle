1ac0e6c34c22d86faf554ae9ca02b8ed016ed982
gustavomiarka <gustavo_miarka@hotmail.com>
2022-05-15 20:39:14 -0300

    modificacion archivos html y css creacion de funciones

---
+++ js/agregarpalabra.js
@@ -0,0 +1,25 @@
+let palabras = ["perro", "gato", "avion", "casa", "pelota"];
+
+function agregarPalabra(){
+    let input = document.querySelector("input").value;
+    if (!palabras.includes(input) && (input != "") ) {
+        palabras.push(input);
+        document.querySelector("input").value = "";      
+    } else if (input == ""){
+        alert("Campo vacio. Ingrese una palabra");
+    } else {
+        alert("Palabra ya existente, intente con otra");
+    }
+}
+
+agregar.addEventListener("click", agregarPalabra);
+
+
+
+function selecPalabra(){
+    let palabra = palabras[Math.floor(Math.random() * palabras.length)];
+    return palabra;
+}
+
+init.addEventListener("click", selecPalabra);
+
