2a3d271b98964b8141f48502eab4298c78a62167
gustavomiarka <gustavo_miarka@hotmail.com>
2022-05-18 00:16:24 -0300

    se agregan modificaciones html css js

--- js/juego.js
+++ js/juego.js
@@ -1,2 +1,12 @@
+// let palabraConGuiones = secretWord.replace(/./g, " _ ");
 
 
+// function selecPalabra(){
+//     let interno = document.querySelector(".caja-secreto").innerHTML;
+//     interno = JSON.parse(localStorage.getItem("palabras"))[Math.floor(Math.random()*(JSON.parse(localStorage.getItem("palabras"))).length)];
+//     return interno;
+// }
+
+// var init = document.querySelector("#init");
+
+// init.addEventListener("click", selecPalabra);
