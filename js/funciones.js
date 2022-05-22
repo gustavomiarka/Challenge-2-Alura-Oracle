4a854cbc2c891da50ef725be0e1efa9f585701a0
gustavomiarka <gustavo_miarka@hotmail.com>
2022-05-22 01:38:49 -0300

    desarrollo de ultimas funciones y purificacion de codigo

--- js/funciones.js
+++ js/funciones.js
@@ -4,9 +4,17 @@ var palabras = ["PERRO", "GATO", "AVION", "CASA", "PELOTA", "CABALLO", "MESA"];
 
 /*------------------------------------------------------------------------------*/
 
+
+// declaracion de variables
 var newLocal = JSON.parse(localStorage.getItem("palabras"));
 
-let palabra;
+let palabra =  newLocal[Math.round(Math.random()*(newLocal).length)];
+let index = 0; 
+let cuerpo = document.getElementsByClassName("cuerpo")
+
+String.prototype.replaceAt=function(index, character) { return this.substr(0, index) + character + this.substr(index+character.length); }
+/*------------------------------------------------------------------------------*/
+
 
 // funcion para validar la palabra que se coloque en el input.
 function validarPalabra(){
@@ -80,57 +88,99 @@ var keyValue;
 function validarLetra(){
     document.addEventListener('keydown', (event) => {
     keyValue = event.key;
-    let regexp = /^[a-zA-Z]+$/g;
+    let regexp = /^[A-Z]+$/g;
     if (regexp.test(keyValue)) {
-        return console.log(keyValue);
+        console.log(keyValue)
+        // dibujarLetraCorrecta();
     } else {
         return alert("caracter no permitido, solo letras");
+        // dibujarLetraIncorrecta();
     }
   });
 };
 /*------------------------------------------------------------------------------*/
 
-//   caja.outerText.charAt()
 
-
-// let contadorFallos = 0;
-// document.querySelector("#init").addEventListener("click", () =>{
-    
-//     // let haFallado = true;
-//     for(const i in palabra){
-//         if(keyValue == palabra[i]){
-//             palabraSinGuiones = palabra.replace(i, keyValue)
-//             console.log(palabraSinGuiones)
-//         }
-//     }
-// })
-
-// if (haFallado) {
-//     contadorFallos++;
-//     // incorporar imagenes
-//     if (contadorFallos == 4) {
-//         alert("perdites")
-//     }
-// }else {
-//     if (palabraConGuion.indexof('_') <0) {
-//         alert("victoria");
-//     }
-// }
-// document.querySelector("").innerHTML = palabraConGuion;
-
-// document.querySelector().value = '';
-// document.querySelector().focus();
-
-
+// funcion para comprobar que la letra tecleada sea la correcta
 function dibujarLetraCorrecta(){
-    for (const i of palabra) {
+    let palabraSinGuion = document.querySelector(".caja-secreto")
+    for (const i in palabra) {
         if(keyValue == palabra[i]){
-            document.querySelector(".caja-secreto").innerHTML = palabra.replace(i*2, keyValue)
+            alert("Exito")
+            palabraSinGuion.innerHTML = palabraSinGuion.textContent.replaceAt(i*2, keyValue);
         }
     }
 }
+/*------------------------------------------------------------------------------*/
 
+
+// funcion para comprobar que la letra tecleada no esta incluida en la palabra secreta
 function dibujarLetraIncorrecta(){
-
+    let contador = 4;
+    for (const i in palabra) {
+        if(keyValue != palabra[i]){
+            alert("upsss")
+            document.querySelector(".caja-error").innerHTML = document.querySelector(".caja-error").textContent.replaceAt(i, keyValue);
+            contador--;
+        } else {
+            alert("perdiste")
+        }
+    }
 }
+/*------------------------------------------------------------------------------*/
 
+
+// funcion para verificar fin del juego
+function verificarFinJuego(){
+    let perdedor = false
+    if(document.querySelector(".caja-error").textContent.indexOf('_') <0){
+        alert("Perdiste");
+        perdedor = true;
+        return  perdedor
+    } else {
+        return perdedor;
+    }
+}
+/*------------------------------------------------------------------------------*/
+
+
+// funcion para mostrar mensaje para fin del juego
+function mensajePerdedor(){
+    if (verificarFinJuego()) {
+        alert("Fin del Juego!!")
+    }
+}
+/*------------------------------------------------------------------------------*/
+
+
+// funcion para verificar ganador
+function verificarGanador(){
+    let ganador = false
+    if(document.querySelector(".caja-secreto").textContent.indexOf('_') <0){
+        alert("Victoria");
+        ganador = true;
+        return  ganador
+    } else {
+        return ganador;
+    }
+}
+/*------------------------------------------------------------------------------*/
+
+
+// funcion para mostrar mensaje ganador
+function mensajeGanador(){
+    if (verificarGanador()) {
+        alert("Felicidades Ganaste!!")
+    }
+}
+/*------------------------------------------------------------------------------*/
+
+
+// funcion para dibujar horca por cada error que cometa el jugador
+function dibujarHorca(){
+    if (index < cuerpo.length) {
+        cuerpo.item(index).style.display = "flex";
+        index++;
+    }
+}
+/*------------------------------------------------------------------------------*/
